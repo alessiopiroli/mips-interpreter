177f55e9dbf3c35e685c0521dadc71934a61a29b
Alessio Piroli <alessiopiroli@Alessios-MacBook-Air.local>
2025-08-30 21:46:09 +0200

    Formatted correct_inputs asm

--- corrected_inputs_outputs/asms/loop-fixed.asm
+++ corrected_inputs_outputs/asms/loop-fixed.asm
@@ -1,8 +1,8 @@
 .text
 main:
-llo $a1, 10000
+    llo $a1, 10000
 begin:
-trap print_int
-addi $a0, $a0, 1
-bne $a0, $a1, begin
-trap exit
+    trap print_int
+    addi $a0, $a0, 1
+    bne $a0, $a1, begin
+    trap exit
